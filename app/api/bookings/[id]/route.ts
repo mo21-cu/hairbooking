b3d30be65eb1217994c05175a9d777bf5d375704
mo21-cu <mehi7654321@gmail.com>
2025-01-26 06:47:45 +0100

    Fix prisma generation and update authOptions imports 3.0

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../auth/[...nextauth]/authOptions"
+import { authOptions } from "@/lib/auth"
 
 export async function PUT(request: Request, { params }: { params: { id: string } }) {
   const session = await getServerSession(authOptions)
